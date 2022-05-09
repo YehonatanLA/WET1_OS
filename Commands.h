7230af6f86b7fbfd91e93a9befaca88f83eb7b79
Adi <adibot10>
2022-05-09 18:42:30 +0300

    checking fg4

--- Commands.h
+++ Commands.h
@@ -259,6 +259,16 @@ public:
 };
 
 
+class EmptyCommand : public BuiltInCommand {
+public:
+    EmptyCommand(const char *cmd_line);
+
+    virtual ~EmptyCommand() {}
+
+    void execute() override;
+};
+
+
 class SmallShell {
 private:
     // TODO: Add your data members
--- signals.cpp
+++ signals.cpp
@@ -10,7 +10,7 @@ using namespace std;
 void ctrlZHandler(int sig_num) {
 	// TODO: Add your implementation
     cout<< "smash: got ctrl-Z" << endl;
-    SmallShell& smash = SmallShell::getInstance(b );
+    SmallShell& smash = SmallShell::getInstance();
     int pid = smash.curr_cmd->getCommandPid();
     if (pid == NO_PID)
     {
@@ -39,8 +39,9 @@ void ctrlCHandler(int sig_num) {
     }
     else
     {
-        kill(pid, KILL_SIGNAL_NUM);
+        kill(pid, 9);
         cout<< "smash: process " << pid << " was killed" << endl;
+        return;
 
     }
 }
